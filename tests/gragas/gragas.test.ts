381c8077f3a8f0460c14729808a9ee9cc0a067ee
Stian Kodehode <stian.taule123@gmail.com>
2024-02-04 00:47:15 +0100

    added env for api key

--- tests/gragas/gragas.test.ts
+++ tests/gragas/gragas.test.ts
@@ -1,11 +1,15 @@
 import Gragas from '../../src/gragas';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import {
   SummonerAPI,
   MatchAPI,
   StatusAPI,
 } from '../../src/endpoints/endpoints';
 test('gragas', async () => {
-  const gragas = new Gragas('API_KEY', 'EUW');
+  const apiKey = process.env.RIOT_API_KEY || '';
+  const gragas = new Gragas(apiKey, 'EUW');
   const summoner = await gragas.summoner.getSummonerByName('T1 Faker', 'KR');
   console.log(summoner);
 });
