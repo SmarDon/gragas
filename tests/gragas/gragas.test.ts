9be74383e94ad462309b5a19fabaa0252951058e
Stian Kodehode <stian.taule123@gmail.com>
2024-02-04 01:29:46 +0100

    scuffed test

--- tests/gragas/gragas.test.ts
+++ tests/gragas/gragas.test.ts
@@ -1,15 +1,17 @@
+// @ts-nocheck
+
 import Gragas from '../../src/gragas';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import {
-  SummonerAPI,
-  MatchAPI,
-  StatusAPI,
-} from '../../src/endpoints/endpoints';
 test('gragas', async () => {
   const apiKey = process.env.RIOT_API_KEY || '';
+
   const gragas = new Gragas(apiKey, 'EUW');
-  const summoner = await gragas.summoner.getSummonerByName('T1 Faker', 'KR');
-  console.log(summoner);
+
+  const summoner =
+    (await gragas.summoner.getSummonerByName('SmarDon', 'EUW')) || null;
+  const matches = await gragas.match.getMatchesByPuuid(summoner?.puuid, 20, 0);
+  const match = await gragas.match.getMatchById(matches[0]);
+  console.log(match);
 });
