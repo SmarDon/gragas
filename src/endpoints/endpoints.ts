8988f19a7f5b70596a6c74c9ea2f69da10933e50
Stian Kodehode <stian.taule123@gmail.com>
2024-02-04 02:33:03 +0100

    add champion mastery and account to import/export

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,5 +1,7 @@
 import SummonerAPI from './summoner';
 import MatchAPI from './match';
 import StatusAPI from './lolStatus';
+import ChampionMasteryAPI from './championMastery';
+import AccountAPI from './account';
 
-export { SummonerAPI, MatchAPI, StatusAPI };
+export { SummonerAPI, MatchAPI, StatusAPI, ChampionMasteryAPI, AccountAPI };
