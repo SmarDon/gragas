5e55f913eff7a0e8bab45d4a09230971fb285623
Stian Kodehode <stian.taule123@gmail.com>
2024-02-04 14:30:16 +0100

    add league-exp endpoint

--- src/endpoints/leagueExp_V4.ts
+++ src/endpoints/leagueExp_V4.ts
@@ -1,2 +1,35 @@
 import axios from 'axios';
 import { PLATFORM_BASE_URLS, LEAGUE_EXP_V4 } from '../helpers/constants';
+
+export default class LeagueExp_V4 {
+  private apiKey: string;
+  public region: string;
+
+  constructor(apiKey: string, region: string = 'EUW') {
+    this.apiKey = apiKey;
+    this.region = region;
+  }
+
+  async getLeagueExp(
+    queue: string,
+    tier: string,
+    division: string,
+    region: string
+  ): Promise<any> {
+    const URL =
+      PLATFORM_BASE_URLS[region || this.region] +
+      LEAGUE_EXP_V4 +
+      `${queue}/${tier}/${division}`;
+    try {
+      const response = await axios.get(URL, {
+        headers: {
+          'X-Riot-Token': this.apiKey,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching challenger league:', error);
+    }
+    return null;
+  }
+}
