199dd763851e58685c4af98cd0399437175eed13
Stian Kodehode <stian.taule123@gmail.com>
2024-02-04 14:10:23 +0100

    import/export league api

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -3,5 +3,13 @@ import MatchAPI from './match_V5';
 import StatusAPI from './lolStatus_V4';
 import ChampionMasteryAPI from './championMastery_V4';
 import AccountAPI from './account_V1';
+import LeagueAPI from './league_V4';
 
-export { SummonerAPI, MatchAPI, StatusAPI, ChampionMasteryAPI, AccountAPI };
+export {
+  SummonerAPI,
+  MatchAPI,
+  StatusAPI,
+  ChampionMasteryAPI,
+  AccountAPI,
+  LeagueAPI,
+};
